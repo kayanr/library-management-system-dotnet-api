27f5fbab33872f838816040027e2cbe8254c9b23
Kayan <kayan.reid@gmail.com>
2026-03-13 13:37:36 -0400

    Fix duplicate LibraryDbContext definition

--- LibraryManagement.Api/Program.cs
+++ LibraryManagement.Api/Program.cs
@@ -3,16 +3,20 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Add services to the container.
 builder.Services.AddControllers();
 
+var dbPath = Path.Combine(builder.Environment.ContentRootPath, "library.db");
+
 builder.Services.AddDbContext<LibraryDbContext>(options =>
-    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlite($"Data Source={dbPath}"));
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -26,3 +30,4 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+

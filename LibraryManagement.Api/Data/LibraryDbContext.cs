aa819ee347abcb65c07d7489fc1510e49271357b
Kayan <kayan.reid@gmail.com>
2026-03-21 12:33:47 -0400

    Add Loan model and register in DbContext

--- LibraryManagement.Api/Data/LibraryDbContext.cs
+++ LibraryManagement.Api/Data/LibraryDbContext.cs
@@ -12,6 +12,7 @@ namespace LibraryManagement.Api.Data
 
         public DbSet<Book> Books { get; set; }
         public DbSet<Member> Members { get; set; }
+        public DbSet<Loan> Loans { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
